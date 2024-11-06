22670502fc0d2cafb7de85b6386171242603e98e
TatianePink <tpink@faeterj-petropolis.edu.br>
2024-11-05 21:48:38 -0300

    novapgn

--- finalReact/src/components/Header/index.jsx
+++ finalReact/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import * as styles from './Header.module.css';
 
 export default function Header() {
--- finalReact/src/main.jsx
+++ finalReact/src/main.jsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/Global.css';
-import App from './App.jsx';
+// import App from './App.jsx';
 import Header from './components/Header/index.jsx';
 import Footer from './components/Footer/index.jsx';
-import Home from './pages/Home/index.jsx';
-import Carousel from './components/Carousel/index.jsx';
+import Error from './pages/Error';
+// import Home from './pages/Home/index.jsx';
+// import Carousel from './components/Carousel/index.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
-    <Home />
-    <Carousel/>
+    <Error/>
+    {/* <Home />
+    <Carousel/> */}
     <Footer/>
   </StrictMode>,
 )
